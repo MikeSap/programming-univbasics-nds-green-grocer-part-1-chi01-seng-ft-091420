fe533963a7bba9244a6b33743b1c691b7f1163ab
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-09 21:21:10 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,7 +3,7 @@ require 'pry'
 def find_item_by_name_in_collection(name, collection)
  
  collection.each do |item|
-   item.each do |name, val|
+   item.each do |property, val|
    binding.pry
   end
  end
