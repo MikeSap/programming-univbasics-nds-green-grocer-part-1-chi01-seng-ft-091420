b5a994fe343d8c74a61371ad4f8f41c3347d0bcd
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-09 22:28:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -15,7 +15,7 @@ end
 def consolidate_cart(cart)
  consolidated_cart = []
  cart.each do |item|
-   if find_item_by_name_in_collection (item[:item], collection) == nil
+   if find_item_by_name_in_collection (name, collection) == nil
      item[:count] = 1
    else
      item[:count] = item[:count] + 1
