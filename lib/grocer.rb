93e1fa71db79d147332ddb9ce5d2f076ca1da882
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-09 21:38:30 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,8 +5,7 @@ def find_item_by_name_in_collection(name, collection)
  collection.each do |key|
   key.each do |prop, food|
     if food == name
-      shopping = []
-     shopping << key
+      shopping = key
    end
   end
  end
